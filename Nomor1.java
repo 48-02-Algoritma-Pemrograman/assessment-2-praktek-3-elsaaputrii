db9fc42e10c3b12cd4c92a97d32fd06de20a7a92
elsaaputrii <elsaputri1113@gmail.com>
2024-12-12 13:54:36 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,13 +1,38 @@
 import java.util.Scanner;
 
 public class Nomor1 {
+    public static double hitungLuasSegitiga(double alas, double tinggi) {
+        return 0.5 * alas * tinggi;
+    }
+
+    public static void fizzBuzz (int N) {
+        for (int i = 0; i <= N; i++) {
+            if (i % 15 == 0) {
+                System.out.print("FizzBuzz ");
+            } else if (i % 3 == 0) {
+                System.out.print("Fizz ");
+            } else if (i % 5 == 0) {
+                System.out.print("Buzz ");
+            } else {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println();
+    }
 
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan nilai alas: ");
+        double alas = scanner.nextDouble();
+        System.out.println("Masukkan nilai tinggi: ");
+        double tinggi = scanner.nextDouble();
+        
+        double luas = hitungLuasSegitiga(alas, tinggi);
+        System.out.println("Luas segitiga adalah: " + luas);
+        
+        System.out.println("Masukkan nilai N: ");
         int N = input.nextInt();
-        /* Pekerjaan anda mulai dari sini */
-
-        /* Pekerjaan anda berakhir sini */
+        fizzBuzz(N);
 
         System.out.println(Nomor1.class.getDeclaredMethods().length); 
     }
